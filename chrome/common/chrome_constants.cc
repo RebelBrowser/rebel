a5513900746c059481d2056e02d44a0060be7851
Timothy Flynn <timothy.flynn@viasat.com>
2022-06-08 18:37:36 -0400

    Branding: Minimum changes to compile with Rebel branding on macOS

--- chrome/common/chrome_constants.cc
+++ chrome/common/chrome_constants.cc
@@ -17,6 +17,9 @@
 #define PRODUCT_STRING "Google Chrome"
 #elif BUILDFLAG(USE_INTERNAL_CHROME_FOR_TESTING_ICONS)
 #define PRODUCT_STRING "Google Chrome for Testing"
+#elif BUILDFLAG(REBEL_BROWSER)
+#include "rebel/build/buildflag.h"
+#define PRODUCT_STRING REBEL_STRING_BUILDFLAG(REBEL_BROWSER_NAME)
 #elif BUILDFLAG(CHROMIUM_BRANDING)
 #define PRODUCT_STRING "Chromium"
 #else
