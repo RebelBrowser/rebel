21d55110228499d5068002a20e4773733f3cc965
Timothy Flynn <timothy.flynn@viasat.com>
2022-06-21 14:06:44 -0400

    Branding: Display chrome:// URLs with a Rebel-branded schema
    This essentially creates an alias from the Rebel-branded schema (e.g.
    rebel://) to the Chrome schema (chrome://). The Chrome schema itself is
    just too pervasive to replace wholesale, so treating the Rebel-branded
    schema as an alias avoid breaking the browser; we just need to ensure
    the places it is displayed to the user becomes branded.

--- ios/chrome/browser/url/url_util.mm
+++ ios/chrome/browser/url/url_util.mm
@@ -18,6 +18,11 @@
 #import "url/gurl.h"
 #import "url/url_constants.h"
 
+#include "build/branding_buildflags.h"  // Needed for REBEL_BROWSER.
+#if BUILDFLAG(REBEL_BROWSER)
+#include "rebel/components/url_formatter/rebel_constants.h"
+#endif
+
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
@@ -44,6 +49,9 @@ bool IsHandledProtocol(const std::string& scheme) {
   DCHECK_EQ(scheme, base::ToLowerASCII(scheme));
   return (scheme == url::kHttpScheme || scheme == url::kHttpsScheme ||
           scheme == url::kAboutScheme || scheme == url::kDataScheme ||
+#if BUILDFLAG(REBEL_BROWSER)
+          scheme == rebel::kRebelScheme ||
+#endif
           scheme == kChromeUIScheme);
 }
 
