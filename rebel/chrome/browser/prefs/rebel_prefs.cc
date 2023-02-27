e515dfc0e5caf5a17be17b39584361e6cb2a7f02
Timothy Flynn <timothy.flynn@viasat.com>
2023-02-27 14:47:18 -0500

    Updater: Integrate Sparkle for macOS upgrades

--- rebel/chrome/browser/prefs/rebel_prefs.cc
+++ rebel/chrome/browser/prefs/rebel_prefs.cc
@@ -15,6 +15,10 @@
 #include "rebel/chrome/browser/ntp/remote_ntp_theme_provider.h"
 #endif
 
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
+#include "rebel/chrome/browser/channel_selection.h"
+#endif
+
 #include "rebel/chrome/browser/ntp/remote_ntp_icon_storage.h"
 #include "rebel/services/network/remote_ntp_api_allow_list.h"
 
@@ -23,6 +27,10 @@ namespace rebel {
 void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   rebel::RemoteNtpApiAllowList::RegisterProfilePrefs(registry);
   rebel::RemoteNtpIconStorage::RegisterProfilePrefs(registry);
+
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
+  rebel::RegisterChannelSelectionProfilePrefs(registry);
+#endif
 
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
   ntp_tiles::CustomLinksManagerImpl::RegisterProfilePrefs(registry);
