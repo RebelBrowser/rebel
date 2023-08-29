23337c787b0d170b1ddddc15044ef89bb3b0edbd
dzhang <debin.zhang@viasat.com>
2023-08-29 09:59:18 -0400

    allow localhost to get OGS hints to setup early connection to hint server

--- components/optimization_guide/core/hints_fetcher.cc
+++ components/optimization_guide/core/hints_fetcher.cc
@@ -7,6 +7,7 @@
 #include <memory>
 #include <utility>
 
+#include "build/branding_buildflags.h"
 #include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
@@ -458,7 +459,13 @@ std::vector<GURL> HintsFetcher::GetSizeLimitedURLsForFetching(
                         " URL:", urls[i].possibly_invalid_spec()}));
       break;
     }
+#if BUILDFLAG(REBEL_BROWSER)
+    // This change is to allow WPT to fetch hints from localhost (127.0.0.0/orange.html) so that 
+    // connection between Rebel and OGS hint server can be established early.
+    if (IsValidURLForURLKeyedHint(urls[i]) || net::IsLocalhost(urls[i]) || urls[i].HostIsIPAddress()) {
+#else
     if (IsValidURLForURLKeyedHint(urls[i])) {
+#endif // BUILDFLAG(REBEL_BROWSER)
       valid_urls.push_back(urls[i]);
     } else {
       OPTIMIZATION_GUIDE_LOG(
